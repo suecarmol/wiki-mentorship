f1c29956d720db7ead027a76b86caca7e6913058
suecarmol <scardenasmolinar@wikimedia.org>
2021-05-19 14:21:14 -0500

    Add models and migrations

---
+++ WikiMentor/WikiMentor/topics/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from WikiMentor.users.models import UserProfile
+
+
+class Topic(models.Model):
+    class Meta:
+        app_label = "topics"
+        verbose_name = "topic"
+        verbose_name_plural = "topics"
+
+    name = models.CharField(max_length=235, help_text="The name of the topic")
+    description = models.TextField(help_text="A long description of that topic")
+    potential_mentors = models.ManyToManyField(UserProfile, related_name="mentors")
+    potential_mentees = models.ManyToManyField(UserProfile, related_name="mentees")
